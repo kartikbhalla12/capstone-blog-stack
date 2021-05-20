6a6cb7561496bd2456bb59f18abc7c3848880a85
Kartik Bhalla <kartikbhalla12@gmail.com>
2021-05-21 02:00:18 +0530

    config: update api endpoint
    Signed-off-by: Kartik Bhalla <kartikbhalla12@gmail.com>

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-export const apiEndpoint = `https://k8ub8s8i0m.execute-api.ap-south-1.amazonaws.com/dev`
+export const apiEndpoint = `blog-api.kartikbhalla.dev`
 // export const apiEndpoint = `localhost:3000/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
   domain: 'dev-gg9z0y7i.us.auth0.com', // Auth0 domain
   clientId: 'm2QIn0r3RzS9JyfJeNwW4aP8M2SrVY1a', // Auth0 client id
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: `${window.location.origin}/callback`
 }
